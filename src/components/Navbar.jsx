5dd933e77ac00e76d8735c0df2aafc14fb613c34
Himanshu <githubhimanshu0@gmail.com>
2025-05-02 02:34:46 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <>
       {/* Desktop Navbar */}
-      <div className="flex justify-between lg:mx-36 items-center lg:justify-between m-[auto]">
+      <div className="flex justify-between lg:mx-36 items-center lg:justify-between">
         <div>
           <img src={mylogo} alt="Logo" className="md:h-20 h-15 lg:h-24" />
         </div>
@@ -18,7 +18,7 @@ const Navbar = () => {
             <a href="#"><li className="mx-3 relative text-black after:content-[''] after:absolute after:left-1/2 after:bottom-[-4px] after:h-[2px] after:w-0 after:bg-blue-700 after:transition-all after:duration-300 after:ease-in-out after:-translate-x-1/2 hover:after:w-[150%] cursor-pointer hidden md:block hover:text-blue-900">Contact</li></a>
           </ul>
         </div>
-        <a href=""><h1 className="font-semibold border-[3px] md:px-6 md:py-2 px-5 mx-2 py-3.5 text-lg md:text-base hidden md:block">Get in touch</h1></a>
+        <a href="/"><h1 className="font-semibold border-[3px] md:px-6 md:py-2 px-3 py-1 text-lg md:text-base">Get in touch</h1></a>
         {/* Mobile Menu Hamburger  */}
         <MobileMenu></MobileMenu>
       </div>
